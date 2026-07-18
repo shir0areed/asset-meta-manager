ceb5793ed52b6886f7db967db41851add5ee613d
Shiro <dawnblackwolf.microsoft@gmail.com>
2026-07-18 16:30:51 +0900

    start.pyへ移行

--- start.bat
+++
@@ -1,4 +0,0 @@
-cd /d "%~dp0"
-
-call .\venv\Scripts\activate.bat
-python -m app.main --identity "%~1"
---
+++ start.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+import pathlib
+import venv
+import os
+
+
+APP_MODULE = 'app.main'
+
+
+if __name__ == '__main__':
+    app_dir = pathlib.Path(__file__).resolve().parent
+    venv_dir = app_dir / 'venv'
+    requirements_txt = app_dir / 'requirements.txt'
+    os.chdir(app_dir)
+
+    # python -m venv .venv
+    builder = venv.EnvBuilder(with_pip=True)
+    builder.create(str(venv_dir))
+    context = builder.ensure_directories(str(venv_dir))
+    venv_python = context.env_exe
+
+    # pip install -r requirements.txt
+    subprocess.check_call(
+        [venv_python, '-m', 'pip', 'install', '-r', str(requirements_txt)],
+    )
+
+    # python main.py <args>
+    subprocess.check_call(
+        [venv_python, '-m', APP_MODULE, '--identity', sys.argv[1]] + sys.argv[2:]
+    )
