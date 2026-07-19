fc64750f8abc9e8a6322f15dbeef8730c3ec39b8
Shiro <dawnblackwolf.microsoft@gmail.com>
2026-07-19 12:45:53 +0900

    wheel用のstart.py追加

---
+++ dist/start.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+import pathlib
+import venv
+import socket
+
+
+APP_MODULE = 'asset_meta_manager'
+
+
+def find_free_port():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('', 0))
+        return s.getsockname()[1]
+
+
+if __name__ == '__main__':
+    script_dir = pathlib.Path(__file__).resolve().parent
+    venv_dir = script_dir / 'venv'
+
+    port = find_free_port()
+    server_proc = subprocess.Popen([
+        sys.executable,
+        "-m", "http.server",
+        str(port),
+        "--directory", str(script_dir)
+    ])
+
+    index_url = f"http://localhost:{port}/"
+   
+    # python -m venv .venv
+    builder = venv.EnvBuilder(with_pip=True)
+    builder.create(str(venv_dir))
+    context = builder.ensure_directories(str(venv_dir))
+    venv_python = context.env_exe
+
+    # pip install -r requirements.txt
+    subprocess.check_call(
+        [
+            venv_python,
+            '-m',
+            'pip',
+            'install',
+            APP_MODULE.replace('_', '-'),
+            '--find-links',
+            index_url
+        ]
+    )
+
+    server_proc.terminate()
+
+    # python main.py <args>
+    subprocess.check_call(
+        [venv_python, '-m', APP_MODULE] + sys.argv[1:]
+    )
